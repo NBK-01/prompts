1a68c79a1a00c990976eeea33d130807c8e540b5
NBK-01 <nayefkanaan03@gmail.com>
2024-03-01 17:52:47 +0200

    feat: design components

--- src/app/_components/ui/button.tsx
+++ src/app/_components/ui/button.tsx
@@ -8,14 +8,16 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "~/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounde-sm text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default:
-          "bg-neutral-800 text-neutral-50 shadow hover:bg-neutral-800/90",
+          "bg-primary text-neutral-50 shadow hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          success: 
+          "bg-green-500 text-primary-foreground shadow hover:bg-green-500/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
@@ -25,8 +27,8 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-9 px-4 py-2",
-        sm: "h-8 rounded px-3 text-xs",
-        lg: "h-10 rounded px-8",
+        sm: "h-8  px-3 text-xs",
+        lg: "h-10  px-8",
         icon: "h-9 w-9",
       },
     },
